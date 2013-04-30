00825e9c61284726fe6b007a3c12777a502ebc59
Nikolai Weibull <now@bitwi.se>
2013-04-30 12:59:22 +0200

    Inventory-Rake-Tasks-YARD 1.3.3

--- lib/inventory-rake-tasks-yard-1.0/version.rb
+++ lib/inventory-rake-tasks-yard-1.0/version.rb
@@ -3,15 +3,15 @@
 require 'inventory-1.0'
 
 class Inventory::Rake::Tasks::YARD
-  Version = Inventory.new(1, 3, 2){
+  Version = Inventory.new(1, 3, 3){
     def dependencies
       super + Inventory::Dependencies.new{
         development 'lookout', 3, 0, 0
         development 'lookout-rake', 3, 0, 0
-        runtime 'inventory-rake', 1, 3, 0
+        development 'yard-heuristics', 1, 1, 0
+        runtime 'inventory-rake', 1, 4, 0
         runtime 'rake', 0, 9, 2, :feature => 'rake'
         optional 'yard', 0, 8, 0
-        optional 'yard-heuristics', 1, 1, 0
       }
     end
 
